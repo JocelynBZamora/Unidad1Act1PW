997d313644c1e35cf50e0802954b98850bcf3eea
operador <operador@CCA5-020>
2023-09-27 15:58:28 -0600

    Agregar archivos de proyecto.

---
+++ Unidad1Act1PW/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Unidad1Act1PW.Models;
+
+namespace Unidad1Act1PW.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            MiPerfilViewModel vm = new();
+            vm.Nombre = "Jocelyn Berenice Zamora De la cerda";
+            return View(vm);
+        }
+        public IActionResult MiPerfil() 
+        {
+            MiPerfilViewModel vm = new();
+            vm.Nombre = "Jocelyn Berenice Zamora De la cerda";
+            vm.Carrera = "Ing. Sistemas Computacionales";
+            vm.Correo = "201g0293@rcarbonifera.tecnm.mx";
+            vm.Semestre = 7;
+            return View(vm);
+        }
+    }
+}
---
+++ Unidad1Act1PW/Models/MiPerfilViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Unidad1Act1PW.Models
+{
+    public class MiPerfilViewModel
+    {
+        public string? Nombre { get; set; }
+        public string? Carrera { get; set; }
+        public string? Correo { get; set; }
+        public int Semestre { get; set; }
+    }
+}
